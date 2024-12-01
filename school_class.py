4f075e1aa15114785026ac28fadd770ccd82873a
Admin <preciouspeter3173@gmail.com>
2024-12-01 11:51:06 +0100

    school class data added

--- school_class.py
+++ school_class.py
@@ -4,13 +4,10 @@ school_class_data = {
     "Name":["Precious", "Thomas", "Sarah", "Laura", "Teddy", "Nora"],
     "Age":["60", "20", "25", "30", "35", "40"],
     "Grade":[80, 75, 25, 30, None,None,],
-
 }
 
 df = pd.DataFrame(school_class_data)
-
 df.to_csv(r"C:\Users\Admin\Downloads\school.csv")
-
 filtered_data = df[df["Grade"] > 70]
 print(filtered_data)
 print()
